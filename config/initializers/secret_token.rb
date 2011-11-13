31cea22edacb556d547899130fa05e4033b8ac3a
Johannes Wagener <johannes@wagener.cc>
2011-11-13 11:56:26 -0800

    empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Storywheel::Application.config.secret_token = '372b5b6cc6a8feb6c81328869eb65c388ad47d2241f08492f253902b92fbd70bfa87f64c006dcc6fa04d49c2aa2d738522f3493497b7d7040a71087972a19512'
