7ce7b9255dce58a60c4f3e72246740409221ddb2
Johannes Wagener <johannes@wagener.cc>
2012-01-11 17:12:28 -0800

    adding shared.js to precompilation

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,7 @@ module Storywheel
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    config.assets.precompile += ['shared.js']
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
