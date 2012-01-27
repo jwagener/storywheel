ccdf62d456001b9a738870c73a9f8394143ccb21
Johannes Wagener <johannes@wagener.cc>
2012-01-27 16:34:29 -0500

    fixing soundcloud patch

--- config/initializers/soundcloud_encoding_fix.rb
+++ config/initializers/soundcloud_encoding_fix.rb
@@ -3,9 +3,11 @@ require 'soundcloud'
 class Soundcloud
   def handle_response(refreshing_enabled=true, &block)
     response = block.call
-    parsed_response = HashResponseWrapper.new(::JSON.parse(response.body))
+    parsed_response = ::JSON.parse(response.body)
 
     if parsed_response && !response.success?
+      parsed_response = HashResponseWrapper.new(parsed_response)
+
       if parsed_response.code == 401 && refreshing_enabled && options_for_refresh_flow_present?
         exchange_token
         # TODO it should return the original
